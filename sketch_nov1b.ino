33ed078353c26d17583d0f7db667cb7b48b780ff
graxx07 <147257096+graxx07@users.noreply.github.com>
2023-11-07 11:47:46 -0500

    Update sketch_nov1b.ino

--- sketch_nov1b.ino
+++ sketch_nov1b.ino
@@ -3,7 +3,7 @@
 #define button3 2
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo
+Servo myservo;  
 Servo girl;
 Servo sink;
 
@@ -22,41 +22,41 @@ void loop()
   byte buttonState3 = digitalRead(button3);
 
   if(buttonState1 == LOW){
-    Serial.println("pussy!");
-    myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-    Serial.println("fuck!");
-    myservo.write(135);  // Values below 90 rotate one way, values above 90 rotate the other
+    Serial.println("1");
+    myservo.attach(9);  
+    Serial.println("1 attached");
+    myservo.write(135);  
     delay(2000);
-    Serial.println("shit!");
+    Serial.println("1 moving");
     myservo.write(90);
     delay(100);
-    Serial.println("damn!");
-    myservo.write(45);                 // rotate clockwise full speed
+    Serial.println("1 stop");
+    myservo.write(45);                 
     delay(2000);
-    Serial.println("bitch!");
-    myservo.detach();  // Stop the rotation
+    Serial.println("1 moving");
+    myservo.detach();  
   }
   else if (buttonState2 == LOW){
-    Serial.println("penis");
-    girl.attach(10);  // attaches the servo on pin 9 to the servo object
-    Serial.println("fuck!");
-    girl.write(135);  // Values below 90 rotate one way, values above 90 rotate the other
+    Serial.println("2");
+    girl.attach(10);  
+    Serial.println("2 attached");
+    girl.write(135);  
     delay(2000);
-    Serial.println("shit!");
+    Serial.println("2 moving");
     girl.write(90);
     delay(100);
     girl.detach();
   }
   else if (buttonState3 == LOW){
-    Serial.println("boobs");
-    sink.attach(11);  // attaches the servo on pin 9 to the servo object
-    Serial.println("fuck!");
+    Serial.println("3");
+    sink.attach(11);  
+    Serial.println("3 attached");
     sink.write(85);
     sink.write(-85);
     sink.write(85);
-    sink.write(-85);  // Values below 90 rotate one way, values above 90 rotate the other
+    sink.write(-85);  
     delay(2000);
-    Serial.println("shit!");
+    Serial.println("3 moving");
     sink.detach();
   }
   else {
